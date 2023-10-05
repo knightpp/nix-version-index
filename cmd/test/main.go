bce8f00ea3426c230990744429acf6b36e11ee1f
Danylo Kondratiev <knightpp@proton.me>
2023-10-05 23:19:23 +0300

    WIP

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
+	"github.com/gocql/gocql"
 	"github.com/knightpp/nix-version-index/internal/attrset"
 )
 
@@ -23,37 +25,157 @@ func (c Commit) String() string {
 }
 
 var (
-	rev   string
-	hash  string
-	query string
+	rev     string
+	hash    string
+	query   string
+	writeDb bool
 )
 
 func init() {
 	flag.StringVar(&rev, "rev", "", "git revision")
 	flag.StringVar(&hash, "hash", "", "unpacked tarball hash")
 	flag.StringVar(&query, "query", "", "nix attrset path")
+	flag.BoolVar(&writeDb, "write", false, "write to db")
 }
 
 func main() {
 	flag.Parse()
 
-	if rev != "" || hash != "" {
-		err := evaluate(Commit{Rev: rev, Hash: hash})
-		if err != nil {
-			log.Fatal(err)
-		}
-		return
-	}
-
-	if query == "" {
+	var err error
+	switch {
+	case rev != "" || hash != "":
+		err = evaluate(Commit{Rev: rev, Hash: hash})
+	case query != "":
+		err = executeQuery(query)
+	case writeDb:
+		err = writeToDb()
+	default:
 		log.Print("no command specified")
-		return
 	}
-
-	err := executeQuery(query)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func createKeyspaceAndTable() error {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "system"
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return fmt.Errorf("create session: %w", err)
+	}
+
+	defer session.Close()
+
+	err = session.Query("CREATE KEYSPACE IF NOT EXISTS versions WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1}").Exec()
+	if err != nil {
+		return fmt.Errorf("create keyspace: %w", err)
+	}
+
+	err = session.Query(`
+		CREATE TABLE IF NOT EXISTS versions.db (
+			rev text,
+			attr text,
+			version text,
+			PRIMARY KEY ((attr,rev),version)
+		) 
+	`).Exec()
+	if err != nil {
+		return fmt.Errorf("create table: %w", err)
+	}
+
+	err = session.Query(`CREATE INDEX IF NOT EXISTS rev_index ON versions.db (rev)`).Exec()
+	if err != nil {
+		return fmt.Errorf("create rev index: %w", err)
+	}
+
+	err = session.Query(`CREATE INDEX IF NOT EXISTS attr_index ON versions.db (attr)`).Exec()
+	if err != nil {
+		return fmt.Errorf("create attr index: %w", err)
+	}
+
+	return nil
+}
+
+func writeToDb() error {
+	err := createKeyspaceAndTable()
+	if err != nil {
+		return fmt.Errorf("create keyspace and table: %w", err)
+	}
+
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "versions"
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return fmt.Errorf("create session: %w", err)
+	}
+
+	defer session.Close()
+
+	dirEntries, err := os.ReadDir("packages")
+	if err != nil {
+		return fmt.Errorf("read dir: %w", err)
+	}
+
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			continue
+		}
+
+		rev, _, _ := strings.Cut(entry.Name(), "-")
+		var count int
+
+		err = session.Query("SELECT COUNT(*) FROM db WHERE rev = ?", rev).Scan(&count)
+		if err != nil {
+			return fmt.Errorf("query count: %w", err)
+		}
+
+		if count != 0 {
+			continue
+		}
+
+		data, err := os.ReadFile(filepath.Join("packages", entry.Name()))
+		if err != nil {
+			return err
+		}
+
+		var set attrset.Set
+		err = json.Unmarshal(data, &set)
+		if err != nil {
+			return err
+		}
+
+		batch := session.NewBatch(gocql.UnloggedBatch)
+
+		var i int
+		attrToVersion := attrset.Flatten(set)
+		for attr, version := range attrToVersion {
+			i++
+			if i%1000 == 0 {
+				fmt.Printf("processing %d/%d\n", i, len(attrToVersion))
+			}
+
+			if batch.Size() >= 500 {
+				err = session.ExecuteBatch(batch)
+				if err != nil {
+					return fmt.Errorf("execute batch: %w", err)
+				}
+
+				batch = session.NewBatch(gocql.UnloggedBatch)
+			}
+
+			batch.Query("INSERT into db (rev,attr,version) VALUES (?, ?, ?)", rev, attr, version)
+		}
+
+		err = session.ExecuteBatch(batch)
+		if err != nil {
+			return fmt.Errorf("execute batch: %w", err)
+		}
+	}
+
+	return nil
 }
 
 func executeQuery(query string) error {
--- internal/attrset/attrset.go
+++ internal/attrset/attrset.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 type Set map[string]PackageOrSet
@@ -47,4 +48,31 @@ func (ps *PackageOrSet) UnmarshalJSON(data []byte) error {
 	}
 
 	return nil
+}
+
+func Flatten(s Set) map[string]string {
+	m := make(map[string]string, length(s, 0))
+	flatten(s, m)
+	return m
+}
+
+func flatten(s Set, m map[string]string, prefixes ...string) {
+	for attr, v := range s {
+		if v.Package != nil {
+			m[strings.Join(append(prefixes, attr), ".")] = v.Package.Version
+		} else if len(v.Set) != 0 {
+			flatten(v.Set, m, append(prefixes, attr)...)
+		}
+	}
+}
+
+func length(s Set, l int) int {
+	for _, v := range s {
+		if v.Set != nil {
+			l += length(v.Set, l)
+		} else if v.Package != nil {
+			l++
+		}
+	}
+	return l
 }
